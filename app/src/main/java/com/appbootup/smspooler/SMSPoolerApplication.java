5b392168afa75627e7969de68d9d18fe2e430161
Sachin S R <sachin.r.r@gmail.com>
2016-07-22 20:16:44 +0530

    #update - add MultiDex and io.branch key

--- app/src/main/java/com/appbootup/smspooler/SMSPoolerApplication.java
+++ app/src/main/java/com/appbootup/smspooler/SMSPoolerApplication.java
@@ -1,12 +1,20 @@
 package com.appbootup.smspooler;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.crashlytics.android.Crashlytics;
 
 import io.fabric.sdk.android.Fabric;
 
 public class SMSPoolerApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
@@ -17,5 +25,5 @@ public class SMSPoolerApplication extends Application {
                 .build();
 
         Fabric.with(fabric);
-   }
+    }
 }
